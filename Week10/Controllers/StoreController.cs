91551c0c57b92e1c69237c3d09942004bffb1ad5
ifotn <rich@infrontofthenet.com>
2016-11-15 11:38:31 -0500

    end of class files

---
+++ Week10/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week10.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: /Store
+        public String Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        // GET: /Store/Browse
+        public String Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+
+        // GET: /Store/Details
+        public String Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+            return message;
+        }
+    }
+}
